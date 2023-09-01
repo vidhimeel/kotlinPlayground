1ccfcee08c30a2364c73c9ccb68f257c9f155852
vidhimeel <vidhimeel@gmail.com>
2023-09-01 21:56:44 +0530

    To calculate positive or negative number.

---
+++ src/main/kotlin/ControlFlow.kt
@@ -0,0 +1,12 @@
+fun main() {
+
+    val number = -13
+    if (number >= 0) {
+        print("it is positive number")
+
+    }
+    else {
+        print ("it is negative number ")
+    }
+    println()
+}
