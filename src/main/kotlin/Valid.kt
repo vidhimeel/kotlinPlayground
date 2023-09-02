147d613fd6320bfbf712522c802bd821c15aa8fe
vidhimeel <vidhimeel@gmail.com>
2023-09-02 20:50:26 +0530

    Eligible for voting or not

---
+++ src/main/kotlin/Valid.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val mobileNumber = 7878293983
+    if (mobileNumber == 7878293983){
+
+        print(" yes it is valid")
+    }
+    else{
+        print(" sorry it is not valid")
+    }
+    println()
+}
