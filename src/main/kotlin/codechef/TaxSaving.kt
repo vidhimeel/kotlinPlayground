cb3ca8a11548a4fe9cde1f28b39994c8893a778a
vidhimeel <vidhimeel@gmail.com>
2023-09-10 14:51:08 +0530

    TaxSaving solved.

---
+++ src/main/kotlin/codechef/TaxSaving.kt
@@ -0,0 +1,17 @@
+package codechef
+
+fun main() {
+
+    val testCase = readln().toInt()
+
+    repeat(testCase){
+
+        val input = readln().split(" ")
+
+        val a = input[0].toInt()
+        val b = input[1].toInt()
+
+        println(a-b)
+    }
+
+}
