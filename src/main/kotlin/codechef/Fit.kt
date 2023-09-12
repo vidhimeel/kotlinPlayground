32dde68662ced58300d4a5161d4fadc75d212de2
vidhimeel <vidhimeel@gmail.com>
2023-09-12 21:12:41 +0530

    Fit Solved.

---
+++ src/main/kotlin/codechef/Fit.kt
@@ -0,0 +1,15 @@
+package codechef
+
+fun main() {
+    val testCase = readln().toInt()
+
+    repeat(testCase){
+
+        val input = readln().split(" ")
+
+        val a = input[0].toInt()
+
+        println(a*10)
+    }
+
+}
