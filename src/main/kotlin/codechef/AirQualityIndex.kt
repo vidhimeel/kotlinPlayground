0586e90abaf64bff849ca6b062c9fb8c4d6a3401
vidhimeel <vidhimeel@gmail.com>
2023-09-13 22:20:40 +0530

    Air Quality Index Solved.

--- src/main/kotlin/codechef/AirQualityIndex.kt
+++ src/main/kotlin/codechef/AirQualityIndex.kt
@@ -2,15 +2,8 @@ package codechef
 
 fun main() {
 
-    val cafeMeasured = readln().toInt()
-
-    repeat(cafeMeasured){
-
-        val input = readln().split(" ")
-
-        val x= input[0].toInt()
-
-        if (x < 100 ){
+    val x = readln().toInt()
+    if (x < 100 ){
             println("YES")
         } else {
             println("NO")
@@ -19,4 +12,3 @@ fun main() {
     }
 
 
-}
