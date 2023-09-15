a9fbb7d168e435d0c30da72511717261e45ee7f8
vidhimeel <vidhimeel@gmail.com>
2023-09-15 20:40:12 +0530

    DonDrive Completed.

---
+++ src/main/kotlin/DonDrive.kt
@@ -0,0 +1,18 @@
+fun main() {
+
+    val testCase = readln().toInt()
+
+    repeat(testCase) {
+
+
+        val input = readln().split(" ")
+
+        val a = input[0].toInt()
+        val b = input[1].toInt()
+
+        println(a-b)
+    }
+
+}
+
+
