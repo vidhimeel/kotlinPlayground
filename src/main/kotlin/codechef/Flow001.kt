82b8b60395fe9fcaa6b0eb9137792594c8179135
vidhimeel <vidhimeel@gmail.com>
2023-09-15 19:54:31 +0530

    Flow001 Updated.

--- src/main/kotlin/codechef/Flow001.kt
+++ src/main/kotlin/codechef/Flow001.kt
@@ -4,16 +4,17 @@ fun main() {
 
     val testCase = readln().toInt()
 
-    repeat(testCase){
+    repeat(testCase) {
+
 
         val input = readln().split(" ")
 
         val a = input[0].toInt()
         val b = input[1].toInt()
 
-            println(a+b)
-        }
-
+        println(a+b)
     }
+
+}
 
 
