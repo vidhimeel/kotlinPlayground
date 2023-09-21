4bfdd8f7944004cfdc52c9a598c8c7fa7cb36f77
vidhimeel <vidhimeel@gmail.com>
2023-09-21 22:27:40 +0530

    WorriorChef Updated.

---
+++ src/main/kotlin/codechef/WorriorChef.kt
@@ -0,0 +1,16 @@
+package codechef
+
+fun main() {
+
+    val testCase = readln().toInt()
+
+    repeat(testCase) {
+
+        val input = readln().split(" ")
+
+        val a = input[0].toInt()
+        val b = input[1].toInt()
+
+        println(a - b)
+    }
+}
