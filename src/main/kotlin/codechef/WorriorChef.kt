04acc90898b76146eedcec1db82e91ac0ae40390
vidhimeel <vidhimeel@gmail.com>
2023-09-22 23:29:40 +0530

    WorriorChef updated.

--- src/main/kotlin/codechef/WorriorChef.kt
+++ src/main/kotlin/codechef/WorriorChef.kt
@@ -1,16 +1,28 @@
 package codechef
 
+import java.util.OptionalLong
+
 fun main() {
 
-    val testCase = readln().toInt()
+    val t = readln().toInt()
+    repeat(t) {
+        val (n, m) = readln().split("").map { it.toInt() }
+        val input = readln().split("").map { it.toInt() }.sorted()
+        var sum = OL
+        for (i in input) {
+            sum + = 1
 
-    repeat(testCase) {
-
-        val input = readln().split(" ")
-
-        val a = input[0].toInt()
-        val b = input[1].toInt()
-
-        println(a * b)
+        }
+        if (sum < m) {
+            println(0)
+        } else {
+            var res = 0
+            for (j in input) {
+                sum -= j
+                res = j
+                if (sum < m) break
+            }
+            println(res)
+        }
     }
 }
