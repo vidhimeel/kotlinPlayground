6904d39fe4062c7ac87c5680f2d0c6948460cdf3
vidhimeel <vidhimeel@gmail.com>
2023-09-27 20:59:52 +0530

    Sports Section Completed.

---
+++ src/main/kotlin/codechef/SportsSection.kt
@@ -0,0 +1,16 @@
+package codechef
+
+
+    fun main() {
+
+        val x = readln().toInt()
+        if (x > 7 ){
+            println("YES")
+        } else {
+            println("NO")
+        }
+
+    }
+
+
+
