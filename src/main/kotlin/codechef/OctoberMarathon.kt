b59a35c5cdf5941ae76068329f7f38deac152fae
vidhimeel <vidhimeel@gmail.com>
2023-10-11 22:29:15 +0530

    Testing updated.

---
+++ src/main/kotlin/codechef/OctoberMarathon.kt
@@ -0,0 +1,17 @@
+
+
+fun main() {
+
+    val x = readln().toInt()
+    if (x < 3) {
+        println("GOLD")
+    } else if (x < 6) {
+        println("Silver")
+    } else {
+        println("BRONZE")
+
+    }
+}
+
+
+
