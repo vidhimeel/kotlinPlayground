61b0b036d9bb384e4cc5c7287b2a3a9671024b5d
vidhimeel <vidhimeel@gmail.com>
2023-10-15 20:08:48 +0530

    Reach on top completed.

---
+++ src/main/kotlin/codechef/ReachOnTop.kt
@@ -0,0 +1,16 @@
+package codechef
+
+fun main() {
+    val testCase = readln().toInt()
+
+    repeat(testCase) {
+
+        val x = readln().toInt()
+        if (x >= 30) {
+            println("YES")
+        } else {
+            println("NO")
+        }
+
+    }
+}
