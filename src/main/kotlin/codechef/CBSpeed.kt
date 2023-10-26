c26a4a640b89065161052c8821e642d56eeb511d
vidhimeel <vidhimeel@gmail.com>
2023-10-26 21:13:49 +0530

    CBSpeed Completed.

---
+++ src/main/kotlin/codechef/CBSpeed.kt
@@ -0,0 +1,16 @@
+package codechef
+
+fun main() {
+
+        val input = readln().split(" ")
+        val a = input[0].toInt()
+        val b = input[1].toInt()
+
+        if (a < b) {
+            println("yes")
+        } else {
+            println("no")
+        }
+        println()
+    }
+
