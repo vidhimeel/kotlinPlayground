b0c3342c7613af10155fc848898504b1980bb980
vidhimeel <vidhimeel@gmail.com>
2023-10-26 21:12:56 +0530

    CanDivide Completed.

---
+++ src/main/kotlin/codechef/CanDivide.kt
@@ -0,0 +1,16 @@
+package codechef
+
+fun main() {
+    val testCase = readln().toInt()
+
+    repeat(testCase) {
+
+        val n = readln().toInt()
+        if ( (n == 1 || n % 3 == 0)) {
+            println("Yes")
+        }else  {
+            println("No")
+        }
+
+    }
+}
