1b3101e40cec798381b619b45a932a07244b7c01
vidhimeel <vidhimeel@gmail.com>
2023-10-26 21:34:55 +0530

    Par2 Completed.

---
+++ src/main/kotlin/codechef/Par2.kt
@@ -0,0 +1,16 @@
+package codechef
+
+fun main() {
+    val testCase = readln().toInt()
+
+    repeat(testCase) {
+
+        val n = readln().toInt()
+        if ( n % 2 == 0) {
+            println("Yes")
+        }else  {
+            println("No")
+        }
+
+    }
+}
