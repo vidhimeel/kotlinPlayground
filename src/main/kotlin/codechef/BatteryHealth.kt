e4ec1395437509ef01bd8cfb310aa21b5322cb43
vidhimeel <vidhimeel@gmail.com>
2023-10-27 21:08:33 +0530

    Battery health Completed.

---
+++ src/main/kotlin/codechef/BatteryHealth.kt
@@ -0,0 +1,16 @@
+package codechef
+
+fun main() {
+    val testCase = readln().toInt()
+
+    repeat(testCase) {
+
+        val x = readln().toInt()
+        if ( x >= 80) {
+            println("Yes")
+        }else  {
+            println("No")
+        }
+
+    }
+}
