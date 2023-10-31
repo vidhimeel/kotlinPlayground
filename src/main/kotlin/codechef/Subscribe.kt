8a9d43db701a39f7b0f95c8c70e23bfef2237604
vidhimeel <vidhimeel@gmail.com>
2023-10-31 20:24:17 +0530

    Subscribe Completed.

---
+++ src/main/kotlin/codechef/Subscribe.kt
@@ -0,0 +1,17 @@
+package codechef
+
+fun main() {
+    val testCase = readln().toInt()
+
+    repeat(testCase) {
+
+        val x = readln().toInt()
+        if (x > 30) {
+            println("YES")
+        } else {
+            println("NO")
+        }
+
+    }
+}
+
