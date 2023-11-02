4274e9383eae7fa33586e8d6e827faf23988c949
vidhimeel <vidhimeel@gmail.com>
2023-11-02 21:18:41 +0530

    Prize Pool Completed.

---
+++ src/main/kotlin/codechef/PrizePool.kt
@@ -0,0 +1,16 @@
+package codechef
+
+fun main() {
+
+    val testCase = readln().toInt()
+
+    repeat(testCase) {
+        val input = readln().split(" ")
+
+        val x = input[0].toInt()
+        val y = input[1].toInt()
+
+        println((x * 10) + (y * 90))
+    }
+}
+
