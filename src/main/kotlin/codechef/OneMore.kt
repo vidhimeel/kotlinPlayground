78e17504f5aa8005d76f0ad15eae8da879b4a80e
vidhimeel <vidhimeel@gmail.com>
2023-11-07 17:18:40 +0530

    One More Completed.

---
+++ src/main/kotlin/codechef/OneMore.kt
@@ -0,0 +1,16 @@
+package codechef
+
+fun main() {
+    val testCase = readln().toInt()
+
+    repeat(testCase) {
+
+        val n = readln().toInt()
+        if ( n > 24) {
+            println("Yes")
+        }else  {
+            println("No")
+        }
+
+    }
+}
