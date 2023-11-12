ba82fc6912e7cf5fd0835532a84eb44fdb86b6ce
vidhimeel <vidhimeel@gmail.com>
2023-11-12 21:45:32 +0530

    Equal distribution completed.

---
+++ src/main/kotlin/codechef/EqualDistribution.kt
@@ -0,0 +1,19 @@
+package codechef
+
+fun main() {
+
+    val testCase = readln().toInt()
+
+    repeat(testCase) {
+        val input = readln().split(" ")
+
+        val a = input[0].toInt()
+        val b = input[1].toInt()
+
+        if((a + b) % 2 == 0){
+            println("YES")
+        } else {
+            println("NO")
+        }
+    }
+}
