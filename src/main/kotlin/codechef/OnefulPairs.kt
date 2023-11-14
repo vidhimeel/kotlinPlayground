43a92b40eb42107dc1e016bd76277fa0496c4fc9
vidhimeel <vidhimeel@gmail.com>
2023-11-14 22:19:04 +0530

    Oneful pair completed.

---
+++ src/main/kotlin/codechef/OnefulPairs.kt
@@ -0,0 +1,15 @@
+package codechef
+
+fun main() {
+
+        val input = readln().split(" ")
+
+        val a = input[0].toInt()
+        val b = input[1].toInt()
+
+        if(a+b+(a*b) == 111){
+            println("YES")
+        } else {
+            println("NO")
+        }
+    }
