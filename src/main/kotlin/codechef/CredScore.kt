b26bfb1ee5615ac6a52b7692ec4115536969dfc7
vidhimeel <vidhimeel@gmail.com>
2023-11-18 22:09:45 +0530

    Cred Score completed.

---
+++ src/main/kotlin/codechef/CredScore.kt
@@ -0,0 +1,10 @@
+package codechef
+
+fun main() {
+    val input = readln().toInt()
+        if (input >= 750) {
+            println("YES")
+        } else {
+            println("NO")
+        }
+    }
