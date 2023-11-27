17d462441a4990be405e3d02ce7bd39a9d7c75db
vidhimeel <vidhimeel@gmail.com>
2023-11-27 19:28:47 +0530

    Flip Card Completed.

---
+++ src/main/kotlin/codechef/FlipCard.kt
@@ -0,0 +1,18 @@
+package codechef
+
+fun main() {
+    val testCase = readln().toInt()
+
+    repeat(testCase) {
+
+        val input = readln().split(" ")
+        val a = input[0].toInt()
+        val b = input[1].toInt()
+
+        if(a-b >b){
+            println(b)
+        }else{
+            println(a-b)
+        }
+    }
+}
