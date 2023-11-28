75558ac19c3d0361e01c039d71c570d0ccbab8a7
vidhimeel <vidhimeel@gmail.com>
2023-11-28 20:04:25 +0530

    Chef Apps Completed.

---
+++ src/main/kotlin/codechef/ChefApps.kt
@@ -0,0 +1,22 @@
+package codechef
+
+fun main() {
+    val testCase = readln().toInt()
+
+    repeat(testCase) {
+
+        val input = readln().split(" ")
+        val a = input[0].toInt()
+        val b = input[1].toInt()
+        val c = input[2].toInt()
+        val d = input[3].toInt()
+
+        if(a >= b+c+d){
+            println(0)
+        }else if(a >= b+d || a >= c+d){
+            println(1)
+        } else {
+            println(2)
+        }
+    }
+}
