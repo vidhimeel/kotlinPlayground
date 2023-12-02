7a100b26f99f412c535d74468975ecc25ca3f7d1
vidhimeel <vidhimeel@gmail.com>
2023-12-02 22:08:24 +0530

    Speed limit test Completed.

---
+++ src/main/kotlin/codechef/SpeedLimitTest.kt
@@ -0,0 +1,22 @@
+package codechef
+
+fun main() {
+    val testCase = readln().toInt()
+
+    repeat(testCase) {
+
+        val input = readln().split(" ")
+        val a = input[0].toInt()
+        val b = input[1].toInt()
+        val c = input[2].toInt()
+        val d = input[3].toInt()
+
+        if(a/b > c/d){
+            println("ALICE")
+        }else if(a/b < c/d){
+            println("BOB")
+        }else{
+            println("EQUAL")
+        }
+    }
+}
