87647a809b3e3a6b93295eef783ca2a4e6e64bb4
vidhimeel <vidhimeel@gmail.com>
2023-12-09 20:47:37 +0530

    Fruit Chaat Completed.

---
+++ src/main/kotlin/codechef/FruitChaat.kt
@@ -0,0 +1,19 @@
+package codechef
+
+fun main() {
+    val testCase = readln().toInt()
+
+    repeat(testCase) {
+
+        val input = readln().split(" ")
+
+        val a = input[0].toInt()
+        val b = input[1].toInt()
+
+        if(a/2 > b){
+            println(b)
+        }else{
+            println(a/2)
+        }
+    }
+}
