b15ad9b5f2a44458b945a57c168141328df6d6ac
vidhimeel <vidhimeel@gmail.com>
2023-12-10 20:43:34 +0530

    Consonant Or Vowel Completed.

---
+++ src/main/kotlin/codechef/ConsonantOrVowel.kt
@@ -0,0 +1,13 @@
+package codechef
+
+fun main() {
+    val a : Char = readln()[0]
+    if(a=='A' || a=='E' || a=='I' || a=='O' || a=='U')
+    {
+        println("Vowel")
+    }
+    else
+    {
+        println("Consonant")
+    }
+}
