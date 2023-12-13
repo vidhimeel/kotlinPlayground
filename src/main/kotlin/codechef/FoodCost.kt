521d9a9c0437bc4bf68ad3ed460d169d1eacda1b
vidhimeel <vidhimeel@gmail.com>
2023-12-13 23:10:16 +0530

    Food Cost Completed.

---
+++ src/main/kotlin/codechef/FoodCost.kt
@@ -0,0 +1,14 @@
+package codechef
+fun main() {
+
+    val input = readln().split(" ")
+
+    val a = input[0].toInt()
+    val b = input[1].toInt()
+
+    val result = (a * 6 + b)
+    println(result)
+
+}
+
+
