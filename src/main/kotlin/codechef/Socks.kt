ab383cd6fd01a74a604cce6a5687da833af1511d
vidhimeel <vidhimeel@gmail.com>
2023-12-14 23:39:41 +0530

    Socks completed.

---
+++ src/main/kotlin/codechef/Socks.kt
@@ -0,0 +1,14 @@
+package codechef
+
+fun main() {
+    val input = readln().split(" ")
+
+    val a = input[0].toInt()
+    val b = input[1].toInt()
+    val c = input[2].toInt()
+    if (a == b && b == c) {
+        println("YES")
+    } else {
+        println("No")
+    }
+}
