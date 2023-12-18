9830496c90dc414f89e6a2fa7f35ed0bf1b6bf7f
vidhimeel <vidhimeel@gmail.com>
2023-12-18 21:25:11 +0530

    Party2 completed.

---
+++ src/main/kotlin/codechef/Party2.kt
@@ -0,0 +1,24 @@
+package codechef
+
+fun main() {
+    val testCase = readln().toInt()
+
+    repeat(testCase) {
+
+        val input = readln().split(" ")
+
+        val x = input[0].toInt()
+        val y = input[1].toInt()
+        val z = input[2].toInt()
+
+        if (x * y <= z) {
+            println("YES")
+        } else {
+            println("NO")
+        }
+
+    }
+}
+
+
+
