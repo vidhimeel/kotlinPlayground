ab3825a37844c5f9bd92506b04f9df19283327d7
vidhimeel <vidhimeel@gmail.com>
2023-12-19 19:18:23 +0530

    Fever completed.

---
+++ src/main/kotlin/codechef/Fever.kt
@@ -0,0 +1,17 @@
+package codechef
+
+
+fun main() {
+    val testCase = readln().toInt()
+
+    repeat(testCase) {
+        val x = readln().toInt()
+        if (x <= 98) {
+            println("No")
+        } else {
+            println("Yes")
+        }
+
+    }
+}
+
