fb7a07da686f87070dba730fdf766c26aed4e18c
vidhimeel <vidhimeel@gmail.com>
2023-12-20 21:28:30 +0530

    Thala completed.

---
+++ src/main/kotlin/codechef/Thala.kt
@@ -0,0 +1,13 @@
+package codechef
+
+fun main() {
+
+        val x = readln().toInt()
+        if (x == 7) {
+            println("Thala")
+        } else {
+            println("Sadge")
+        }
+
+    }
+
