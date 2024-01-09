027ae536d9c724ff63de51ebcb176b9953dedd31
vidhimeel <vidhimeel@gmail.com>
2024-01-09 19:44:40 +0530

    Instagram Completed.

---
+++ src/main/kotlin/codechef/Instagram.kt
@@ -0,0 +1,18 @@
+package codechef
+
+fun main() {
+    val testCase = readln().toInt()
+
+    repeat(testCase) {
+
+        val input = readln().split(" ")
+        val x = input[0].toInt()
+        val y = input[1].toInt()
+
+        if(x > y * 10){
+            println("YES")
+        } else{
+            println("NO")
+        }
+    }
+}
