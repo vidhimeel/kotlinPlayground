78ab3310f8056b0c0960d95489e3595af6163dba
vidhimeel <vidhimeel@gmail.com>
2024-01-31 20:33:46 +0530

    Advitiya Completed.

---
+++ src/main/kotlin/codechef/Advitiya.kt
@@ -0,0 +1,11 @@
+package codechef
+
+fun main() {
+    val input = readln().toInt()
+
+    if(input == 16 || input == 17 || input == 18){
+        println("Advitiya")
+    }else{
+        println("Waiting For Advitiya")
+    }
+}
