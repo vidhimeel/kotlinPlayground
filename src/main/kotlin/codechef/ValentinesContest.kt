dcf5a8662dda94df90543cdb762fa93f11c2a729
vidhimeel <vidhimeel@gmail.com>
2024-02-08 20:55:32 +0530

    Valentines Contest Completed.

--- src/main/kotlin/codechef/ValentinesContest.kt
+++ src/main/kotlin/codechef/ValentinesContest.kt
@@ -3,7 +3,7 @@ package codechef
 fun main() {
     val n = readln().toInt()
 
-    if(n == 121 || n == 122 || n == 123){
+    if(n == 121){
         println("Likely")
     }else{
         println("Unlikely")
