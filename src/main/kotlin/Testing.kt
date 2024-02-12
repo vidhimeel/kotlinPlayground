01bda5777677d585b15db521cf0f03be8ba4519a
vidhimeel <vidhimeel@gmail.com>
2024-02-12 21:34:45 +0530

    Testing Completed.

--- src/main/kotlin/Testing.kt
+++ src/main/kotlin/Testing.kt
@@ -4,15 +4,17 @@ fun main() {
     repeat(testCase) {
 
         val input = readln().split(" ")
-        val n = input[0].toFloat()
-        val k = input[1].toFloat()
+        val r1 = input[0].toInt()
+        val r2 = input[1].toInt()
+        val r3 = input[2].toInt()
+        val r4 = input[3].toInt()
+        val r5 = input[4].toInt()
 
-        val a = (Math.floor(n /(k + 1.0)).toInt())
-
-        val b = ((n - a * k).toInt())
-        println(a)
-        println(b)
-
+        if (r1 == 1 || r2 == 1 || r3 == 1 || r4 == 1 || r5 == 1) {
+            println("yes")
+        } else {
+            println("no")
+        }
     }
 }
 
