d693f48ee60286fc3623ba646e180fb296e85106
vidhimeel <vidhimeel@gmail.com>
2024-02-14 15:18:21 +0530

    Testing Completed.

--- src/main/kotlin/Testing.kt
+++ src/main/kotlin/Testing.kt
@@ -1,20 +1,14 @@
 fun main() {
-    val testCase = readln().toInt()
+    val t = readln().toInt()
 
-    repeat(testCase) {
+    repeat(t) {
 
         val input = readln().split(" ")
-        val r1 = input[0].toInt()
-        val r2 = input[1].toInt()
-        val r3 = input[2].toInt()
-        val r4 = input[3].toInt()
-        val r5 = input[4].toInt()
+        val x = input[0].toInt()
+        val y = input[1].toInt()
 
-        if (r1 == 1 || r2 == 1 || r3 == 1 || r4 == 1 || r5 == 1) {
-            println("yes")
-        } else {
-            println("no")
-        }
+        val presses = Math.abs(y - x)
+        println(presses)
     }
 }
 
