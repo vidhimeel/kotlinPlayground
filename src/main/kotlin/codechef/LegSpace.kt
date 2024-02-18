3a92e863ae1a1c54d5e5e72b902f171a5752e417
vidhimeel <vidhimeel@gmail.com>
2024-02-18 21:20:16 +0530

    Leg Space Completed.

---
+++ src/main/kotlin/codechef/LegSpace.kt
@@ -0,0 +1,15 @@
+package codechef
+
+fun main() {
+
+    val input = readln().split(" ")
+
+    val n = input[0].toInt()
+    val m = input[1].toInt()
+    if (n < m){
+        println("YES")
+    } else {
+        println("NO")
+    }
+
+}
