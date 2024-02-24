62e504307ae32c436c4c96452cdaeef0dd3cbef8
vidhimeel <vidhimeel@gmail.com>
2024-02-24 20:14:32 +0530

    Healthy Sleep Completed.

---
+++ src/main/kotlin/codechef/HealthySleep.kt
@@ -0,0 +1,14 @@
+package codechef
+
+fun main(){
+
+    val h = readln().toInt()
+
+    if(h == 8){
+        println("PERFECT")
+    }else if(h > 8){
+        println("MORE")
+    }else{
+        println("LESS")
+    }
+}
