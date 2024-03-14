b3cbf0b8b4180ed2ef074287c3925149d3ad74b2
vidhimeel <vidhimeel@gmail.com>
2024-03-14 21:51:58 +0530

    50-50 Rule Completed.

---
+++ src/main/kotlin/codechef/50-50Rule.kt
@@ -0,0 +1,15 @@
+package codechef
+
+fun main(){
+    val t = readln().toInt()
+    repeat(t){
+        val(x, y) =readln().split(" ").map{it.toInt()}
+        if(x < 50 ){
+            println("Z")
+        }else if(y  < 50 && x >= 50 ){
+            println("F")
+        }else{
+            println("A")
+        }
+    }
+}
