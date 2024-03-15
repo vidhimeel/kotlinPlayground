530d59bbf6f8ff286f812275b81ce27b067f1a1c
vidhimeel <vidhimeel@gmail.com>
2024-03-15 22:06:05 +0530

    50-50 Rule Completed.

---
+++ src/main/kotlin/codechef/50-50Rule2.kt
@@ -0,0 +1,17 @@
+package codechef
+
+fun percentage(x: Int, y: Int): String{
+
+    return if(x < 50) "Z" else if(x >= 50 && y < 50) "F" else "A"
+}
+
+fun main(){
+
+    val t = readln().toInt()
+    repeat(t){
+
+        val(x,y) = readln().split(" ").map{it.toInt()}
+        val percentage = percentage(x, y)
+        println(percentage)
+    }
+}
