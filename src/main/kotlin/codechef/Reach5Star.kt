eb516dee5f86d7d38d4fd25025f90bdfee666521
vidhimeel <vidhimeel@gmail.com>
2024-04-04 22:34:31 +0530

    Reach 5 star Completed.

---
+++ src/main/kotlin/codechef/Reach5Star.kt
@@ -0,0 +1,10 @@
+package codechef
+
+fun willBecome5Star(x: Int, y: Int): String {
+    return if (x + y >= 2000) "YES" else "NO"
+}
+
+fun main() {
+    val (x, y) = readLine()!!.split(" ").map { it.toInt() }
+    println(willBecome5Star(x, y))
+}
