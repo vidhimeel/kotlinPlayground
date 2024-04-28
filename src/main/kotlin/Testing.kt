2a2728ffc16a22bc60097f7181cb51196f89010e
vidhimeel <vidhimeel@gmail.com>
2024-04-28 22:12:55 +0530

    Testing Completed.

--- src/main/kotlin/Testing.kt
+++ src/main/kotlin/Testing.kt
@@ -3,7 +3,7 @@ fun main() {
 
     val x = input[0].toInt()
     val y = input[1].toInt()
-    if(x + y <= 2000 || x - y >= 2000){
+    if(x + y <= 2000 && x - y >= 2000){
         println("YES")
     }else{
         println("NO")
