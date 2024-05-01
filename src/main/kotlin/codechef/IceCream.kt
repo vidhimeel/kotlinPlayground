bc22404707aad1a09f8a549d7e7ff4f7261ee0b3
vidhimeel <vidhimeel@gmail.com>
2024-05-01 20:32:59 +0530

    IceCream Completed.

---
+++ src/main/kotlin/codechef/IceCream.kt
@@ -0,0 +1,17 @@
+package codechef
+
+fun canBuyIceCreams(x: Int, y: Int): String {
+
+    return if (y >= 2 * x) {
+        "YES"
+    } else {
+        "NO"
+    }
+}
+
+fun main() {
+
+    val (x, y) = readln().split(" ").map { it.toInt() }
+
+    println(canBuyIceCreams(x, y))
+}
