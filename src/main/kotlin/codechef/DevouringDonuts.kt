1a65307add3ba4e316c72b156209884c81b2f047
vidhimeel <vidhimeel@gmail.com>
2024-05-08 20:20:14 +0530

    Devouring Donuts Completed.

---
+++ src/main/kotlin/codechef/DevouringDonuts.kt
@@ -0,0 +1,11 @@
+package codechef
+
+fun main() {
+
+    val input = readln().split(" ")
+    val a = input[0].toInt()
+    val b = input[1].toInt()
+
+    val result = (a * b)
+    println(result)
+}
