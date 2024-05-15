c9a8256fb98317a60b3f802e04344a51e483bc2f
vidhimeel <vidhimeel@gmail.com>
2024-05-15 22:38:58 +0530

    Morning Run Completed.

---
+++ src/main/kotlin/codechef/MorningRun.kt
@@ -0,0 +1,16 @@
+package codechef
+
+fun main() {
+
+    val input = readln().split(" ")
+    val x = input[0].toInt()
+    val y = input[1].toInt()
+
+    val result = 2*(x + y)
+    if ( result == 2*(x + y)) {
+        println("YES")
+    } else {
+        println("NO")
+    }
+
+}
