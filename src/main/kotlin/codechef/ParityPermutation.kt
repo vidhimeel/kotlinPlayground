b698324b8523281412236e0c8deae86436e52094
vidhimeel <vidhimeel@gmail.com>
2024-05-17 20:57:57 +0530

    Parity Permutation Completed.

---
+++ src/main/kotlin/codechef/ParityPermutation.kt
@@ -0,0 +1,15 @@
+package codechef
+
+fun main() {
+    val testCase = readln().toInt()
+
+    repeat(testCase){
+
+        val input = readln().split(" ")
+
+        val a1 = input[0].toInt()
+        val a2 = input[1].toInt()
+
+        println((a1 + a2)%2)
+    }
+}
