56db21d8d0655e6960b3351e6d7eb18cb64d4bc5
vidhimeel <vidhimeel@gmail.com>
2024-05-22 21:36:46 +0530

    RCB & CSK Completed.

---
+++ src/main/kotlin/codechef/RCB&CSK.kt
@@ -0,0 +1,15 @@
+package codechef
+
+fun main() {
+
+    val input = readln().split(" ")
+    val x = input[0].toInt()
+    val y = input[1].toInt()
+
+    if (x - y >= 18) {
+        println("RCB")
+    } else {
+        println("CSK")
+    }
+
+}
