605a54ea532f615cb74bac04f8940b1280842918
vidhimeel <vidhimeel@gmail.com>
2024-05-27 21:44:24 +0545

    Football Ties Completed.

--- src/main/kotlin/codechef/RCB&CSK.kt
+++ src/main/kotlin/codechef/RCB&CSK.kt
@@ -6,7 +6,7 @@ fun main() {
     val x = input[0].toInt()
     val y = input[1].toInt()
 
-    if (x - y <= 18) {
+    if (x - y == 18) {
         println("RCB")
     } else {
         println("CSK")
