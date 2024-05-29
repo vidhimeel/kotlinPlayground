9b41837dff483b43299578f233e39023d72bbcb2
vidhimeel <vidhimeel@gmail.com>
2024-05-29 20:15:24 +0530

    How makes P1 Completed.

---
+++ src/main/kotlin/codechef/HowMakesP1.kt
@@ -0,0 +1,14 @@
+package codechef
+
+fun main() {
+
+    val input = readln().split(" ")
+    val a = input[0].toInt()
+    val b = input[1].toInt()
+
+    if (a <= b){
+        println("TYRO")
+    } else {
+        println("DOM")
+    }
+}
