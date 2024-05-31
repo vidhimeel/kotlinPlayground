f62afed585b53ecd8ecf9f0a447a52410ad387e1
vidhimeel <vidhimeel@gmail.com>
2024-05-31 21:44:13 +0530

    Testing Completed.

--- src/main/kotlin/Testing.kt
+++ src/main/kotlin/Testing.kt
@@ -1,11 +1,13 @@
 fun main() {
-    val input = readln().split(" ")
 
+    val input = readln().split(" ")
     val x = input[0].toInt()
     val y = input[1].toInt()
-    if(x + y <= 2000 || y - x >= 2000){
+
+    val result = 2*(x + y)
+    if ( result == 2*(x + y)) {
         println("YES")
-    }else{
+    } else {
         println("NO")
     }
 }
