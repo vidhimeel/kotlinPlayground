0f1d476f6f211aab38e3af3081fabc349e9a2233
vidhimeel <vidhimeel@gmail.com>
2024-06-01 21:08:55 +0530

    Testing Completed.

--- src/main/kotlin/Testing.kt
+++ src/main/kotlin/Testing.kt
@@ -5,7 +5,7 @@ fun main() {
     val b = input[1].toInt()
     val c = input[2].toInt()
 
-    if (a == b || b == c || a == c) {
+    if (a == b || b == c) {
         println("yes")
     } else {
         println("no")
