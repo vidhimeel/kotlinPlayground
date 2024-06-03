1ebd752cabc7dfed21f5e19a84ff54906f56813d
vidhimeel <vidhimeel@gmail.com>
2024-06-03 22:07:40 +0530

    Testing Completed.

--- src/main/kotlin/Testing.kt
+++ src/main/kotlin/Testing.kt
@@ -3,7 +3,6 @@ fun main() {
     val input = readln().split(" ")
     val a = input[0].toInt()
     val b = input[1].toInt()
-    val c = input[2].toInt()
 
     if (a == b ) {
         println("yes")
