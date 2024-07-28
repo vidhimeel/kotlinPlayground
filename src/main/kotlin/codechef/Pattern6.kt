fc3fb328c477bd705fde2cbbebcf8c89e0241a4a
vidhimeel <vidhimeel@gmail.com>
2024-07-28 10:06:23 +0530

    Pattern 6 Completed.

---
+++ src/main/kotlin/codechef/Pattern6.kt
@@ -0,0 +1,15 @@
+package codechef
+
+fun main() {
+    val rows = 5
+
+    for (i in rows downTo 1) {
+        for (space in 1..rows - i) {
+            print("   ")
+        }
+        for (j in 1..i){
+            print("*")}
+        println()
+
+    }
+}
