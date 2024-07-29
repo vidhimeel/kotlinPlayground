237018e3ba39057f65c282cfc6f7892fd472d89d
vidhimeel <vidhimeel@gmail.com>
2024-07-29 11:55:42 +0530

    Pattern8 Completed.

---
+++ src/main/kotlin/Pattern/Pattern8.kt
@@ -0,0 +1,15 @@
+package Pattern
+
+fun main() {
+    repeat(5){ i ->
+        repeat(5){ j->
+
+            val t = if (i == j || i+j == 5-1) "*" else " "
+
+            print(t)
+        }
+
+        println()
+    }
+
+}
