3197a1612cdf8afffc03da68274c66cdaf97db31
vidhimeel <vidhimeel@gmail.com>
2024-07-29 12:42:12 +0530

    Pattern 10 Completed.

---
+++ src/main/kotlin/Pattern/Pattern10.kt
@@ -0,0 +1,20 @@
+package Pattern
+
+fun main() {
+    val rows = 5
+    var k = 0
+
+    for (i in rows downTo 1) {
+        for (space in 1..rows - i) {
+            print("  ")
+        }
+
+        while (k != 2 * i - 1) {
+            print("* ")
+            ++k
+        }
+
+        println()
+        k = 0
+    }
+}
