c3e950ce9370235f7f60ca17a6bbfb40b45328cc
vidhimeel <vidhimeel@gmail.com>
2024-07-31 22:21:53 +0530

    Pattern 16 Completed.

---
+++ src/main/kotlin/Pattern/Pattern16.kt
@@ -0,0 +1,27 @@
+package Pattern
+
+fun main() {
+    val rows = 5
+
+    for (i in 1..rows) {
+        for (space in 1..rows - i) {
+            print("   ")
+        }
+        for (j in 1..i) {
+            print("*")
+        }
+        println()
+
+
+    }
+    for (i in rows downTo 1) {
+        for (space in 1..rows - i) {
+            print("   ")
+        }
+        for (j in 1..i) {
+            print("*")
+        }
+        println()
+
+    }
+}
