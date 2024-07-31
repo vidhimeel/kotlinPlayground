9077ff6e94a38d1c445c31c97797602122f2ad58
vidhimeel <vidhimeel@gmail.com>
2024-07-31 22:22:11 +0530

    Practise 16 Completed.

---
+++ src/main/kotlin/Pattern/Practise.kt
@@ -0,0 +1,15 @@
+package Pattern
+
+fun main() {
+    val rows = 5
+
+    for (i in rows downTo 1) {
+        for (space in 1..rows - i) {
+            print("   ")
+        }
+        for (j in 1..i){
+            print("*")}
+        println()
+
+    }
+}
